f22d8217f4a9a511a073855526ed14044fe93387
lgaieta <lgaieta.dev@gmail.com>
2022-10-22 19:30:50 -0300

    Added basic galeria page

---
+++ src/routes/galeria/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import H1 from '$lib/components/headings/H1.svelte';
+</script>
+
+<svelte:head>
+	<title>Galería - Skyniv</title>
+</svelte:head>
+
+<main class="Main">
+	<div class="TitleContainer"><H1>Galería</H1></div>
+</main>
+
+<style>
+	.Main {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		gap: 2rem;
+	}
+
+	.TitleContainer {
+		margin-top: 3.5rem;
+	}
+</style>
